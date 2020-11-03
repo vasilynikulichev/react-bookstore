df4c204b80beb7a9c3dfa49198a8bc8dc8058552
vasilijnikulicev <vasilynikulichev@gmail.com>
2020-11-03 09:06:27 +0200

    add error for books list

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -4,23 +4,39 @@ import BookListItem from '../book-list-item';
 import Spinner from "../spinner";
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested } from '../../actions';
+import {
+	booksLoaded,
+	booksRequested,
+	booksError,
+} from '../../actions';
 import { compose } from '../../utils';
 
 import './book-list.css';
+import ErrorIndicator from "../error-indicator";
 
 class BookList extends Component {
 	componentDidMount() {
-		const { bookstoreService, booksLoaded, booksRequested } = this.props;
+		const {
+			bookstoreService,
+			booksLoaded,
+			booksRequested,
+			booksError,
+		} = this.props;
 		booksRequested();
-		bookstoreService.getBooks().then(data => booksLoaded(data));
+		bookstoreService.getBooks()
+			.then((data) => booksLoaded(data))
+			.catch((err) => booksError(err));
 	}
 
 	render() {
-		const { books, loading } = this.props;
+		const { books, loading, error } = this.props;
 
 		if (loading) {
-			return <Spinner/>
+			return <Spinner/>;
+		}
+
+		if (error) {
+			return <ErrorIndicator/>;
 		}
 
 		return (
@@ -37,13 +53,18 @@ class BookList extends Component {
 	}
 }
 
-const mapStateToProps = ({ books, loading }) => {
-	return { books, loading };
+const mapStateToProps = ({ books, loading, error }) => {
+	return {
+		books,
+		loading,
+		error,
+	};
 };
 
 const mapDispatchToProps = {
 	booksLoaded,
 	booksRequested,
+	booksError,
 };
 
 export default compose(
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -26,9 +26,13 @@ export default class BookstoreService {
 	];
 
 	getBooks() {
-		return new Promise(resolve => {
+		return new Promise((resolve, reject) => {
 			setTimeout(() => {
-				resolve(this.data);
+				if (Math.random() > 0.75) {
+					reject(new Error(`Can't get books`));
+				} else {
+					resolve(this.data);
+				}
 			}, 700);
 		});
 	}
